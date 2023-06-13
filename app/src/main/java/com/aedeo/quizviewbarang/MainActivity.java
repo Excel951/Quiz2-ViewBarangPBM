528dff8205789b1b9c48087e995cfe85d94590a0
Excel951 <abigailexcelsisdeo@gmail.com>
2023-06-13 23:33:40 +0700

    Quiz2 PBM

---
+++ app/src/main/java/com/aedeo/quizviewbarang/MainActivity.java
@@ -0,0 +1,27 @@
+package com.aedeo.quizviewbarang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    static ListView mListView;
+    static Context mContext;
+    static TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.listview);
+//        mTextView = (TextView) findViewById(R.id.ini);
+        mContext = getApplicationContext();
+
+        GetData getData = new GetData();
+        getData.execute();
+    }
+}
